7e2086e5df06587266834676314c061dea5a7ee5
BrittleSoup3205 <jcoussou80@gmail.com>
2026-08-05 18:57:59 -0500

    Remove photo backup workflow from service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "mileage-logger-split-photo-backup-v38";
+const CACHE_NAME = "mileage-logger-photo-metadata-only-v39";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=split-photo-backup-1",
-  "./app.js?v=split-photo-backup-1",
-  "./inspections.js?v=split-photo-backup-1",
-  "./media-store.js?v=split-photo-backup-1",
+  "./styles.css?v=photo-metadata-only-1",
+  "./app.js?v=photo-metadata-only-1",
+  "./inspections.js?v=photo-metadata-only-1",
+  "./media-store.js?v=photo-metadata-only-1",
   "./vendor/pdf-lib.min.js",
   "./vendor/fflate.min.js",
   "./manifest.webmanifest",
